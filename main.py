e44b7cd29b2677443618ab1350e105e1835716b8
Not Https <nohttpsno@gmail.com>
2024-10-29 16:42:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-# I wanna go to sleep fr
-# how to make some money off of python.
-import (numpy)
-x 0 = 'nig'
-f = 44
-ssh = f ** x
-d = 48 
-sandi = 47
-	while
+# juh testing
+aa = 'she talk too much' 
+ab = 'alo'
+	print(aa ** bb)
