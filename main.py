c5c0fe65a5abe3f861801eec1b19c29619dd376c
Not Https <nohttpsno@gmail.com>
2024-10-29 18:09:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 # juh testing
-aa = 'she talk too much' 
+aa = 'she talk too much ' 
 ab = 'alo'
-	print(aa ** bb)
+print(aa + ab)
+print('this shit is too cringe to be true')
+"""
+ight, border line bordem
+"""
+seeder = int(input('ight who are you?\n'))
+if seeder == 5 :
+	print('shit')
+elif seeder > 5 :
+	print('hehe')
+else :
+	print('yuh we out here')
+# I'm using floating points for money
