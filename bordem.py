71685dce4ddb6d6488ccee11672f9d45b71c4514
Not Https <nohttpsno@gmail.com>
2024-11-03 16:22:05 +0200

    Create bordem.py

---
+++ bordem.py
@@ -0,0 +1,15 @@
+'''
+All the lessons will be seperatly defines as a function.
+'''
+def FloorCNV():
+  Naieve = input('how to put them floors')
+  NotNaieve = int(Naieve) + 1
+  print("OI its",NotNaieve)
+"""
+
+If you are using Vim, hit escape and then type
+
+gg=G
+
+This auto indents everything and will clear up any spaces you have thrown in.
+"""
